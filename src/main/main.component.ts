f9b07087bbdbf8adac5bc6cb546f29cdcc0007b0
solo-leveler <58850415+solo-leveler@users.noreply.github.com>
2023-09-19 13:28:00 +0530

    logc-implementation : api-integration

--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -1,23 +1,41 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent implements OnInit {
-  
-  constructor(private httpClient:HttpClient) { }
+export class MainComponent implements OnInit, AfterViewInit {
+  constructor(private httpClient: HttpClient) {}
+  displayedColumns: string[] = ['Capital', 'Languages', 'Name', 'Population'];
+  dataSource = new MatTableDataSource<CountryModel>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
     this.getCountriesAPI();
   }
 
-  getCountriesAPI(){
-    this.httpClient.get('https://restcountries.com/v3.1/all?fields=name,capital,languages,population').subscribe(res => {
-      console.log(res)
-    })
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
+  getCountriesAPI() {
+    this.httpClient
+      .get<CountryModel[]>(
+        'https://restcountries.com/v3.1/all?fields=name,capital,languages,population'
+      )
+      .subscribe((res) => {
+        this.dataSource.data = res;
+        console.log(this.dataSource.data);
+      });
+  }
+}
+export interface CountryModel {
+  capital: Array<string>;
+  languages: Object;
+  name: Object;
+  population: number;
 }
