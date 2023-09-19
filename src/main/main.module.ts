de2ea97c0cd7fbf61c7e6acab27aa5621fabd3db
solo-leveler <58850415+solo-leveler@users.noreply.github.com>
2023-09-19 12:40:17 +0530

    add required modules

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [MainComponent],
   bootstrap : [MainComponent]
