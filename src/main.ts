1bd36d5fd3cf482593acf4aa39a418c9bd3e0158
solo-leveler <58850415+solo-leveler@users.noreply.github.com>
2023-10-01 23:51:26 +0530

    code-enhancement : added scripts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import 'zone.js/dist/zone';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { MainModule } from './main/main.module';
 
-platformBrowserDynamic().bootstrapModule(MainModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+platformBrowserDynamic()
+  .bootstrapModule(MainModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
